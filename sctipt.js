606b111673b40bffc44490b903b5bdff98f9def2
ROHITH GEDDADA <geddadarohith143@gmail.com>
2026-01-06 13:22:52 +0530

    Add files via upload

---
+++ sctipt.js
@@ -0,0 +1,8 @@
+// button to say hello
+function sayHello(){
+    alert("hello world"); 
+}
+
+let age = 18;
+let canVote = (age >= 18) ? "Yes, you can vote!" : "No, you cannot vote.";
+alert(canVote);
